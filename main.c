ad9d553521fd690ef2dced9e13269e904ca290bc
William Sha <second.yukirhodes@hotmail.fr>
2018-10-02 09:33:20 +0200

    201810020932
    C file change

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+printarray(Anarray)
+int Anarray; {
+printf("%d",Anarray);
+}
+
+main() {
+int Anarray[5]; int i; char c;
+ printarray(Anarray);
+ printarray(Anarray, c, i); }
